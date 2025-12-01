637acbe0fc324d1d43a60488b053577b5117efa2
Max Gosselin <maximgosselin04@gmail.com>
2025-12-01 12:30:55 -0500

    created category table and menu item table

--- finalproject.sql
+++ finalproject.sql
@@ -1,2 +1,20 @@
 Create Database Restaurant;
 use restaurant;
+
+CREATE TABLE category (
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  description TEXT
+);
+CREATE TABLE menu_item (
+  menu_item_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  description TEXT,
+  price DECIMAL(8,2) NOT NULL CHECK (price >= 0),
+  category_id INT NOT NULL,
+  available BOOLEAN DEFAULT TRUE,
+  last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES category(category_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+
