dff4c037a96ab08ed3dd83e31802cb8013ac548a
Serena <Serena.shushack@stu.crc-lennox.qc.ca>
2025-12-03 12:48:03 -0500

    Simplified the operations.

--- CRUD-Ops.sql
+++ CRUD-Ops.sql
@@ -1,70 +1,43 @@
 -- Inserting Categories
 Insert into category (Name, Description)
-value ('Dinner', 'Dinner items');
-
-Insert into category (Name, Description)
-value ('Lunch', 'Lunch items');
-
-Insert into category (Name, Description)
-value ('Breakfast', 'Breakfast items');
-
-Insert into category (Name, Description)
-value ('Sides', 'Sides');
-
-Insert into category (Name, Description)
-value ('Drinks', 'Beverages');
+value
+('Dinner', 'Dinner items'),
+('Lunch', 'Lunch items'),
+('Breakfast', 'Breakfast item'),
+('Sides', 'Sides'),
+('Drinks', 'Beverages');
 
 -- Inserting Menu Items
 Insert into menu_item (name, description, price, category_id, available, last_modified)
-values ('Omelet', 'Egg omelet with choice of toppings', '9.99', 3, 1, now());
-
-Insert into menu_item (name, description, price, category_id, available, last_modified)
-values ('Pizza', '10 inch pizza with choice of toppings', '14.99', 1, 1, now());
-
-Insert into menu_item (name, description, price, category_id, available, last_modified)
-values ('Ruben', 'Roast beef sandwich with mustard', '7.99', 2, 1, now());
-
-Insert into menu_item (name, description, price, category_id, available, last_modified)
-values ('Poutine', 'Fries, topped with gravy and cheese curds', '9.99', 4, 1, now());
-
-Insert into menu_item (name, description, price, category_id, available, last_modified)
-values ('Soda', 'Your choice of basic, diet or zero', '2.99', 5, 1, now());
+values
+('Omelet', 'Egg omelet with choice of toppings', '9.99', 3, 1, now()),
+('Pizza', '10 inch pizza with choice of toppings', '14.99', 1, 1, now()),
+('Ruben', 'Roast beef sandwich with mustard', '7.99', 2, 1, now()),
+('Poutine', 'Fries, topped with gravy and cheese curds', '9.99', 4, 1, now()),
+('Soda', 'Your choice of basic, diet or zero', '2.99', 5, 1, now());
 
 -- Inserting Customers
 Insert into customer (first_name, last_name, phone, email, created_at)
-values ('Emily', 'Carter', '555-214-7832', 'emily.carter@example.com', now());
-
-Insert into customer (first_name, last_name, phone, email, created_at)
-values ('Darien', 'Nguyen', '555-642-1198', 'darien.nguyen@example.com', now());
-
-Insert into customer (first_name, last_name, phone, email, created_at)
-values ('Sophia', 'Martinez', '555-873-4506', 'sophia.martinez@example.com', now());
+values 
+('Emily', 'Carter', '555-214-7832', 'emily.carter@example.com', now()),
+('Darien', 'Nguyen', '555-642-1198', 'darien.nguyen@example.com', now()),
+('Sophia', 'Martinez', '555-873-4506', 'sophia.martinez@example.com', now());
 
 -- Inserting Customer Addresses
 Insert into customer_address (customer_id, street, city, province_state, postal_code, country, is_default)
-values (1, '742 Evergreen Terrace', 'Springfield', 'Illinois', '62704', 'USA', 1);
-
-Insert into customer_address (customer_id, street, city, province_state, postal_code, country, is_default)
-values (2, '123 Maplewood Drive', 'Vancouver', 'British Columbia', 'V6Z1K7', 'Canada', 1);
-
-Insert into customer_address (customer_id, street, city, province_state, postal_code, country, is_default)
-values (3, '742 Evergreen Terrace', 'Sherbrooke', 'Quebec', 'J1G4B5', 'Canada', 1);
+values
+(1, '742 Evergreen Terrace', 'Springfield', 'Illinois', '62704', 'USA', 1),
+(2, '123 Maplewood Drive', 'Vancouver', 'British Columbia', 'V6Z1K7', 'Canada', 1),
+(3, '742 Evergreen Terrace', 'Sherbrooke', 'Quebec', 'J1G4B5', 'Canada', 1);
 
 -- Inserting Employees
 Insert into employee (username, full_name, role, hired_date, active)
-values ('bthompson', 'Benjamin Thompson', 'Cook', '2023-03-18', 1);
-
-Insert into employee (username, full_name, role, hired_date, active)
-values ('lroberts', 'Lillie Roberts', 'Cashier', '2024-07-02', 1);
-
-Insert into employee (username, full_name, role, hired_date, active)
-values ('mfernandez', 'Miguel Fernandez', 'Driver', '2022-09-05', 1);
-
-Insert into employee (username, full_name, role, hired_date, active)
-values ('swhite', 'Sara White', 'Manager', '2021-12-11', 1);
-
-Insert into employee (username, full_name, role, hired_date, active)
-values ('kjones', 'Kevin Jones', 'Other', '2025-01-04', 1);
+values
+('bthompson', 'Benjamin Thompson', 'Cook', '2023-03-18', 1),
+('lroberts', 'Lillie Roberts', 'Cashier', '2024-07-02', 1),
+('mfernandez', 'Miguel Fernandez', 'Driver', '2022-09-05', 1),
+('swhite', 'Sara White', 'Manager', '2021-12-11', 1),
+('kjones', 'Kevin Jones', 'Other', '2025-01-04', 1);
 
 -- Insert Driver
 Insert into Driver (employee_id, vehicle_info, phone)
@@ -72,35 +45,33 @@ values (3, '2020 Honda Civic (Silver)', '555-873-4410');
 
 -- Insert Orders
 Insert into orders (customer_id, address_id, order_datetime, status, total_price, cancelled_reason, delivered_at, last_modified)
-values (1, 1, '2025-12-01 09:10:00', 'Delivered', '35.95', null, '2025-12-01 09:45:00', '2025-12-01 09:45:00');
+values
+(1, 1, '2025-12-01 09:10:00', 'Delivered', '35.95', null, '2025-12-01 09:45:00', '2025-12-01 09:45:00'),
+(2, 2, '2025-12-01 11:05:00', 'Out for Delivery', '39.94', null, null, '2025-12-01 11:30:00'),
+(3, 3, '2025-12-01 12:20:00', 'Cancelled', '22.98', 'Customer Cancellation', null, '2025-12-01 12:30:00');
 
-Insert into orders (customer_id, address_id, order_datetime, status, total_price, cancelled_reason, delivered_at, last_modified)
-values (2, 2, '2025-12-01 11:05:00', 'Out for Delivery', '39.94', null, null, '2025-12-01 11:30:00');
+-- Insert Order Items (Order #1)
+Insert into order_item (order_id, menu_item_id, quantity, unit_price)
+values
+(1, 1, 2, 9.99),
+(1, 5, 2, 2.99),
+(1, 4, 1, 9.99);
 
-Insert into orders (customer_id, address_id, order_datetime, status, total_price, cancelled_reason, delivered_at, last_modified)
-values (3, 3, '2025-12-01 12:20:00', 'Cancelled', '22.98', 'Customer Cancellation', null, '2025-12-01 12:30:00');
+-- Insert Order Items (Order #2)
+Insert into order_item (order_id, menu_item_id, quantity, unit_price)
+values
+(2, 2, 1, 14.99),
+(2, 3, 2, 7.99),
+(2, 5, 3, 2.99);
 
--- Insert Order Items
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (1, 1, 2, 9.99);
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (1, 5, 2, 2.99);
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (1, 4, 1, 9.99);
-
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (2, 2, 1, 14.99);
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (2, 3, 2, 7.99);
-Insert into order_item (order_id, menu_item_id, quantity, unit_price)
-values (2, 5, 3, 2.99);
-
+-- Insert Order Items (Order #3)
 INSERT INTO order_item (order_id, menu_item_id, quantity, unit_price)
-VALUES 
+values
 (3, 2, 1, 14.99),
 (3, 5, 2, 2.99),
 (3, 4, 1, 9.99);
 
+-- Inserting Order Status Log
 Insert into order_status_log (order_id, changed_by,  old_status, new_status, changed_at, comment)
 values
 (1, 'lroberts', null, 'Received', '2025-12-01 09:10:00', 'Order received'), 
@@ -112,6 +83,7 @@ values
 (3, 'lroberts', null, 'Received', '2025-12-01 12:20:00', 'Order received'), 
 (3, 'lroberts', 'Received', 'Cancelled', '2025-12-01 12:25:00', 'Customer cancellation');
 
+-- Inserting Delivery Details
 Insert into delivery (order_id, driver_id, assigned_at, picked_up_at, delivered_at, status, notes)
 values
 (1, 1, '2025-12-01 09:20:00', '2025-12-01 09:30:00', '2025-12-01 09:45:00', 'Delivered', 'Left at front door'), 
